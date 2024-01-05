ae6ef771bdd24d98cceb403085e7711f63e9c938
Eugene Severin <zhokaogurca@gmail.com>
2024-01-06 06:18:22 +0700

    added theory on datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,50 @@
+// We use two types of datatypes:
+// 1: Numneric, 2: Alphabetic and also we could mix them to create alphanumeric data
+
+// Numeric types: int, float
+
+// Alphabetic types: string
+
+// There are three categories of datatypes:
+
+// 1: primitive;  2: userdefined; 3: Derived
+
+// Primive: 
+//          1) Integral: int, char; 
+//          2) bool; 
+//          3) floating point: float, double;
+
+// Userdefines: 
+//          1) enum; 
+//          2) structure;
+//          3) union;
+//          4) class;
+
+// Derived: 
+//          1) Array;
+//          2) Pointer;
+//          3) References;
+
+
+// Datatype(size, range): 
+//                      int (2 to 4, -32768 to 32768),
+//                      float (4, -3.4^38 to 3.4^38),
+//                      double (8, -1.7^308 to 1.7^308),
+//                      char (1, -128 to 128),
+//                      bool (undefined, True/False)
+
+
+// Integral size and rages: 
+//                          int size = 2 to 4 bytes, range = -32768 to 32768 
+//                          char size = 1 byte, range = -128 to 128
+
+// int and chat can be modified. Modifiers are "unsigned", "long"
+
+// Unsigned doubles the range. 0 to 65535 for int, 0 to 256 for char
+// Long extends the size for int and double. It makes int size double: 2 - 4/ 4 - 8
+// As for doubles, it makes it bigger from 8 to 10
+
+
+
+
+
