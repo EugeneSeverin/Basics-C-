d4614dff7a82bdb3a6e75673644fa471bf3ed225
Eugene Severin <zhokaogurca@gmail.com>
2024-01-06 06:32:39 +0700

    added theory on precedence and expressions

---
+++ precedence_and_expressions.cpp
@@ -0,0 +1,17 @@
+// Precedence and expressions
+
+// Precedence
+
+// () - 3 (highest)
+// *, /, % - 2 (middle)
+// +, - - 1 (lowest) 
+
+// area of triangle: a = (b * h)/2 OR 0.5 * (b * h)
+
+// perimeter of a rectangle: p = 2 * (l + b)
+
+// root of Quadratic Eq: 
+//                      # include <cmath>
+//                      r = (-b + sqrt(b*b - 4 * a * c))/(2*a)
+
+// speed: S = (v*v - u*u) / (2*a) OR S = (pov(v,2) - pov(u,2)) / (2*a)
