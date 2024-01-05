58407100f29ee6cfeeaa7d4dbaecd62875a53281
Eugene Severin <zhokaogurca@gmail.com>
2024-01-06 05:29:16 +0700

    initial commit

---
+++ adding_two_numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    int a, b , c;
+
+    std::cout << "Enter Two Numbers: ";
+    std::cin >> a >> b;
+
+    c = a + b;
+    std::cout << "The sum is: " << c << std::endl;
+    
+    return 0;
+}
