eb111c041091ec5133ecd227e8b9efe5723d2fa9
Eugene Severin <zhokaogurca@gmail.com>
2024-01-07 03:33:52 +0700

    Added compound assignment

---
+++ compound_assignment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a = 10, b = 15, c = 20;
+    int sum = 5 + a + b + c;
+    int prod = a * b * c;
+
+    cout <<  "Sum = " << sum << endl << "Prod = " << prod << endl;
+
+    return 0;
+}
