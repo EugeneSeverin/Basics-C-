9b1c50914ce08a97580c821626f69ed0c395de17
Eugene Severin <zhokaogurca@gmail.com>
2024-01-06 07:09:31 +0700

    Added operators and expressions

---
+++ operators_and_arithmetic_expressions.cpp
@@ -0,0 +1,10 @@
+// Operators and expressions
+
+// Arifmetic:   +, -, *, /, % ;
+// Relational:  <, <=, >, >=, == ;
+// Logical:     &&, ::, ! ;
+// Bitwise:     &, :, ~, ^ ;
+// Increment/Decrement:     ++, -- ;
+// Assignment:      =
+
+// % - mod operation. It can't be performed for floats, but can be used with int, char
