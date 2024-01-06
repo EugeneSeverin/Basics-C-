0bd8c0f67dd4d70653bed1f9c6b9e5a90496d74c
Eugene Severin <zhokaogurca@gmail.com>
2024-01-06 07:27:19 +0700

    Added sum_of_first_n_natural_numbers.cpp

---
+++ sum_of_first_n_natural_numbers.cpp
@@ -0,0 +1,19 @@
+// sum = n*(n+1)/2
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n, sum;
+
+    cout << "Enter n" << endl;
+
+    cin >> n;
+
+    sum = n*(n+1)/2;
+    
+    cout << "Sum is: " << sum << endl;
+
+}
